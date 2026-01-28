755a225fb1aa8ec99a83c5305b79e1371c934f09
EmiliaDobrucka-STUDE <EmiliaDobrucka-STUDE@S-DN1H504-SL06>
2026-01-28 13:32:34 +0000

    Add project files.

---
+++ GameLab_Hub/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GameLab_Hub
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
